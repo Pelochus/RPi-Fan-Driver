e226ea3f79abce062e2eccc26c0e545298cf67bf
Pelochus <49835792+Pelochus@users.noreply.github.com>
2021-03-27 18:08:50 +0000

    Add files via upload

---
+++ code/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Title: ./install.sh
+# Author: Pelochus
+# Version: 1.0
+# Brief: Installs RPi-Fan-Driver files
+
+clear
+
+echo
+echo "--------------------------------------------------------------------------------------------"
+echo "RUN THIS AS ROOT (sudo ./install.sh)"
+echo "This scripts installs RPi-Fan-Driver files in your system"
+echo "Before you continue make sure that previous steps mentioned in the instructions are complete"
+echo "This includes calibrating minimum fan speed, GPIO pin used, fan speed and temps steps..."
+echo "If you haven't done this step do it now, else continue"
+echo "Do you want to continue? [Y/N]"
+echo "--------------------------------------------------------------------------------------------"
+echo
+
+read continue
+
+if [ $continue == 'Y' ] || [ $continue == 'y' ]; then
+	sudo apt install rpi.gpio -y
+	sudo mv /rpi-fan-code/fan-control.service /lib/systemd/system
+	sudo mv /rpi-fan-code/rpi-fan-driver.py /usr/bin/rpi-fan-driver
+	sudo systemctl enable fan-control.service
+	echo "-------------------------------------------"
+	echo "Finished. Changes will be applied at reboot"
+	echo "-------------------------------------------"
+else
+	echo "-----------"
+	echo "Exiting now"
+	echo "-----------"
+fi
---
+++ code/rpi-fan-code/minspeed-calibration.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# File: minspeed-calibration.py
+# Author: Pelochus
+# Brief: Calibrates your minimum fan speed 
+
+import RPi.GPIO as GPIO
+import time
+import sys
+
+# Change FAN_PIN if different GPIO is used
+FAN_PIN = 21
+PWM_FREQ = 25
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FAN_PIN, GPIO.OUT, initial=GPIO.LOW)
+fan = GPIO.PWM(FAN_PIN, PWM_FREQ)
+fan.start(0);
+
+print("\n#####################################################################################")
+print("This script is used to calibrate your minimum fan speed\n")
+print("To do this insert various values until the fan starts spinning\n")
+print("Once done, write your value in the main Python file (values should be around 20-40%)\n")
+print("USE CTRL+C TO QUIT\n")
+print("#####################################################################################")
+
+time.sleep(5)
+
+try:
+    while 1:
+        fanSpeed = float(input("Fan Speed (%): "))
+        fan.ChangeDutyCycle(fanSpeed)
+
+except(KeyboardInterrupt):
+    print("Fan Control interrupted by keyboard")
+    GPIO.cleanup()
+    sys.exit()
